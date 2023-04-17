0014ef5aa6a43d475d79ca1005d55eadab0de506
Akshat Shah <ashah1506@gmail.com>
2023-04-17 02:25:19 -0400

    added and uncommented some stuff

--- server/routes.js
+++ server/routes.js
@@ -370,8 +370,140 @@ const similar_recipes = async function (req, res) {
 }
 
 // Route 6: GET /recipes?
-
 const recipes = async function (req, res) {
+  const ingredient_list = req.params.ingredients.split(' ');
+  const max_prep_time = parseInt(req.query.max_prep_time);
+
+  const page = req.query.page;
+  const pageSize = req.query.page_size ?? 10;
+  let query = "";
+  if (!page) {
+    if (ingredient_list.length == 1) {
+      query += `WITH combined_recipes AS 
+        (SELECT Recipe_id 
+        FROM Recipe_Ingredient
+        WHERE Ingredient_id IN
+          (SELECT Ingredient_id
+          FROM Ingredients
+          WHERE Ingredient_name LIKE '%${ingredient_list[0]}%'))
+        
+          SELECT r1.*, AVG(r2.rating) AS avg_rating, COUNT(r2.rating) AS num_reviews
+          FROM combined_recipes c
+          JOIN Recipes r1 ON c.Recipe_id = r1.id
+          LEFT JOIN Reviews r2 ON c.Recipe_id = r2.Recipe_id
+          GROUP BY r1.id
+          ORDER BY AVG(r2.rating), COUNT(r2.rating)`;
+
+    } else if (ingredient_list.length > 1) {
+      query += "WITH ";
+      for (let i = 0; i < ingredient_list.length; i++) {
+        query += `recipes${i} AS
+                    (SELECT Recipe_id 
+                    FROM Recipe_Ingredient
+                    WHERE Ingredient_id IN
+                      (SELECT Ingredient_id
+                      FROM Ingredients
+                      WHERE Ingredient_name LIKE '%${ingredient_list[i]}%')), 
+                  `;
+      }
+
+      query += `combined_recipes AS (
+                  SELECT recipes0.Recipe_id
+                  FROM recipes0 `;
+
+      for (let i = 1; i < ingredient_list.length; i++) {
+        query += `INNER JOIN recipes${i} 
+                  ON recipes${i - 1}.Recipe_id = recipes${i}.Recipe_id `;
+
+        if (i === ingredient_list.length - 1) {
+          query += `) \n`;
+        }
+      }
+
+      query += `SELECT r1.*, AVG(r2.rating) AS avg_rating, COUNT(r2.rating) AS num_reviews
+          FROM combined_recipes c
+          JOIN Recipes r1 ON c.Recipe_id = r1.id
+          LEFT JOIN Reviews r2 ON c.Recipe_id = r2.Recipe_id
+          WHERE r1.num_ingredients >= ${ingredient_list.length} AND r1.preparation_time <= ${max_prep_time} 
+          GROUP BY r1.id
+          ORDER BY AVG(r2.rating), COUNT(r2.rating)`
+    }
+
+    connection.query(query, (err, data) => {
+      if (query === '' || err || data.length === 0) {
+        console.log(err);
+        res.json([]);
+      } else {
+        res.json(data);
+      }
+    });
+  } else {
+    if (ingredient_list.length == 1) {
+      query += `WITH combined_recipes AS 
+        (SELECT Recipe_id 
+        FROM Recipe_Ingredient
+        WHERE Ingredient_id IN
+          (SELECT Ingredient_id
+          FROM Ingredients
+          WHERE Ingredient_name LIKE '%${ingredient_list[0]}%'))
+        
+          SELECT r1.*, AVG(r2.rating) AS avg_rating, COUNT(r2.rating) AS num_reviews
+          FROM combined_recipes c
+          JOIN Recipes r1 ON c.Recipe_id = r1.id
+          LEFT JOIN Reviews r2 ON c.Recipe_id = r2.Recipe_id
+          GROUP BY r1.id
+          ORDER BY AVG(r2.rating), COUNT(r2.rating)`;
+
+    } else if (ingredient_list.length > 1) {
+      query += "WITH ";
+      for (let i = 0; i < ingredient_list.length; i++) {
+        query += `recipes${i} AS
+                    (SELECT Recipe_id 
+                    FROM Recipe_Ingredient
+                    WHERE Ingredient_id IN
+                      (SELECT Ingredient_id
+                      FROM Ingredients
+                      WHERE Ingredient_name LIKE '%${ingredient_list[i]}%')), 
+                  `;
+      }
+
+      query += `combined_recipes AS (
+                  SELECT recipes0.Recipe_id
+                  FROM recipes0 `;
+
+      for (let i = 1; i < ingredient_list.length; i++) {
+        query += `INNER JOIN recipes${i} 
+                  ON recipes${i - 1}.Recipe_id = recipes${i}.Recipe_id `;
+
+        if (i === ingredient_list.length - 1) {
+          query += `) \n`;
+        }
+      }
+
+      query += `SELECT r1.*, AVG(r2.rating) AS avg_rating, COUNT(r2.rating) AS num_reviews
+          FROM combined_recipes c
+          JOIN Recipes r1 ON c.Recipe_id = r1.id
+          LEFT JOIN Reviews r2 ON c.Recipe_id = r2.Recipe_id
+          WHERE r1.num_ingredients >= ${ingredient_list.length} AND r1.preparation_time <= ${max_prep_time} 
+          GROUP BY r1.id
+          ORDER BY AVG(r2.rating), COUNT(r2.rating)
+          LIMIT ${pageSize} `
+    }
+    if (page > 1) {
+      console.log((page - 1) * pageSize);
+      query += `OFFSET ${(page - 1) * pageSize}`;
+    }
+
+    connection.query(query, (err, data) => {
+      if (query === '' || err || data.length === 0) {
+        console.log(err);
+        res.json([]);
+      } else {
+        res.json(data);
+      }
+    });
+  }
+
 
 }
 
@@ -677,8 +809,8 @@ module.exports = {
   contributor,
   prep_time,
   min_rating,
-  // similar_recipes,
-  // recipes,
+  similar_recipes,
+  recipes,
   some_ingredients,
   worst_recipes,
   top_recipes_contributor,
